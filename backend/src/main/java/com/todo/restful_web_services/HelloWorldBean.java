55045494c4b31fb188509717f31c4357eb93ebd2
Andrew Ouellette <AndrewOuellette1993@gmail.com>
2024-11-30 21:54:48 -0500

    Start using backend services in frontend

---
+++ backend/src/main/java/com/todo/restful_web_services/HelloWorldBean.java
@@ -0,0 +1,25 @@
+package com.todo.restful_web_services;
+
+public class HelloWorldBean {
+
+    private String message;
+
+    public HelloWorldBean(String message){
+        this.message = message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorldBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
--- frontend/src/components/todo/WelcomeComponent.jsx
+++ frontend/src/components/todo/WelcomeComponent.jsx
@@ -1,7 +1,18 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import HelloWorldService from '../../api/todo/HelloWorldService';
 
 class WelcomeComponent extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            welcomeMessage: ''
+        }
+        this.retrieveWelcomeMessage = this.retrieveWelcomeMessage.bind(this);
+        this.handleSuccessfulResponse = this.handleSuccessfulResponse.bind(this);
+    }
+
     render() {
         return (
             <div>
@@ -9,8 +20,27 @@ class WelcomeComponent extends Component {
                 <div className="container">
                     Welcome {this.props.params.name}. You can manage your todos <Link to="/todos">here</Link>.
                 </div>
+                <div className="container">
+                    Click here to get a customized welcome message.
+                    <button onClick={this.retrieveWelcomeMessage} className="btn btn-success">Get Welcome Message</button>
+                </div>
+                <div className="container">
+                    {this.state.welcomeMessage}
+                </div>
             </div>
         )
+    }
+
+    retrieveWelcomeMessage() {
+        // HelloWorldService.executeHelloWorldService()
+        // .then(response => this.handleSuccessfulResponse(response));
+
+        HelloWorldService.executeHelloWorldBeanService()
+        .then(response => this.handleSuccessfulResponse(response));
+    }
+
+    handleSuccessfulResponse(response) {
+        this.setState({welcomeMessage: response.data.message});
     }
 }
 
